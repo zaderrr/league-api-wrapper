3b16b596792370ffe4ab030134ba5dc992f43c00
zaderrr <ellisbrown88@gmail.com>
2022-11-08 19:08:57 +0000

    added match searching

--- sources/@types/index.ts
+++ sources/@types/index.ts
@@ -1,88 +1,276 @@
 import fetch from "node-fetch";
 
 export enum Route {
-  BR =	"br",
-  EUNE =	"eun1",
-  EUW =	"euw1",
-  JP =	"jp1",
-  KR =	"kr",
-  LATAN =	"la1",
-  LATAS =	"la2",
-  NA =	"na1",
-  OC =	"oc1",
-  TR =	"tr1",
-  RU =	"ru",
+  BR = "br",
+  EUNE = "eun1",
+  EUW = "euw1",
+  JP = "jp1",
+  KR = "kr",
+  LATAN = "la1",
+  LATAS = "la2",
+  NA = "na1",
+  OC = "oc1",
+  TR = "tr1",
+  RU = "ru",
   AMERICAS = "americas",
   ASIA = "asia",
-  EUROPE= "europe",
-  SEA = "sea",
+  EUROPE = "europe",
+  SEA = "sea"
 }
 
 export type MatchList = {
   matches: string[];
-}
-
-export type MatchListQuery = {
-  startTime: number;
-  endTime: number;
-  queue: number;
-  type: string;
-  start: number;
-  count: number;
-}
-
+};
 export type ApiQueryParams = {
   route: Route;
   apiKey: string;
-  endpoint: string;
-}
-
-export interface SummonerQuery extends ApiQueryParams {
+  endpoint?: string;
   identifier: string;
+};
+export interface MatchListQuery extends ApiQueryParams {
+  startTime?: number;
+  endTime?: number;
+  queue?: number;
+  type?: string;
+  start?: number;
+  count?: number;
+}
 
+export type Match = {
+  metadata: Metadata;
+  info: Info;
+};
+
+type Metadata = {
+  dataVersion: string;
+  matchId: string;
+  participants: string[];
+};
+
+type Info = {
+  gameCreation: number;
+  gameDuration: number;
+  gameEndTimestamp: number;
+  gameId: number;
+  gameMode: string;
+  gameName: string;
+  gameStartTimestamp: number;
+  gameType: string;
+  gameVersion: string;
+  mapId: number;
+  participants:	Participant[],
+  platformId: string;
+  queueId: number;
+  teams:	Team[],
+  tournamentCode: string;
+};
+
+type Participant = {
+  assists:	number,	
+  baronKills:	number,	
+  bountyLevel:	number,	
+  champExperience:	number,	
+  champLevel:	number,	
+  championId:	number,	
+  championName: string,	
+  championTransform:	number,	
+  consumablesPurchased:	number,	
+  damageDealtToBuildings:	number,	
+  damageDealtToObjectives:	number,	
+  damageDealtToTurrets:	number,	
+  damageSelfMitigated:	number,	
+  deaths:	number,	
+  detectorWardsPlaced:	number,	
+  doubleKills:	number,	
+  dragonKills:	number,	
+  firstBloodAssist:	boolean,	
+  firstBloodKill:	boolean,	
+  firstTowerAssist:	boolean,	
+  firstTowerKill:	boolean,	
+  gameEndedInEarlySurrender:	boolean,	
+  gameEndedInSurrender:	boolean,	
+  goldEarned:	number,	
+  goldSpent:	number,	
+  individualPosition:	string,	
+  inhibitorKills:	number,	
+  inhibitorTakedowns:	number,	
+  inhibitorsLost:	number,	
+  item0:	number,	
+  item1:	number,	
+  item2:	number,	
+  item3:	number,	
+  item4:	number,	
+  item5:	number,	
+  item6:	number,	
+  itemsPurchased:	number,	
+  killingSprees:	number,	
+  kills:	number,	
+  lane:	string,	
+  largestCriticalStrike:	number,	
+  largestKillingSpree:	number,	
+  largestMultiKill:	number,	
+  longestTimeSpentLiving:	number,	
+  magicDamageDealt:	number,	
+  magicDamageDealtToChampions:	number,	
+  magicDamageTaken:	number,	
+  neutralMinionsKilled:	number,	
+  nexusKills:	number,	
+  nexusTakedowns:	number,	
+  nexusLost:	number,	
+  objectivesStolen:	number,	
+  objectivesStolenAssists:	number,	
+  participantId:	number,	
+  pentaKills:	number,	
+  perks:	Perks,	
+  physicalDamageDealt:	number,	
+  physicalDamageDealtToChampions:	number,	
+  physicalDamageTaken:	number,	
+  profileIcon:	number,	
+  puuid:	string,	
+  quadraKills:	number,	
+  riotIdName:	string,	
+  riotIdTagline:	string,	
+  role:	string,	
+  sightWardsBoughtInGame:	number,	
+  spell1Casts:	number,	
+  spell2Casts:	number,	
+  spell3Casts:	number,	
+  spell4Casts:	number,	
+  summoner1Casts:	number,	
+  summoner1Id:	number,	
+  summoner2Casts:	number,	
+  summoner2Id:	number,	
+  summonerId:	string,	
+  summonerLevel:	number,	
+  summonerName:	string,	
+  teamEarlySurrendered:	boolean,	
+  teamId:	number,	
+  teamPosition:	string,	
+  timeCCingOthers:	number,	
+  timePlayed:	number,	
+  totalDamageDealt:	number,	
+  totalDamageDealtToChampions:	number,	
+  totalDamageShieldedOnTeammates:	number,	
+  totalDamageTaken:	number,	
+  totalHeal:	number,	
+  totalHealsOnTeammates:	number,	
+  totalMinionsKilled:	number,	
+  totalTimeCCDealt:	number,	
+  totalTimeSpentDead:	number,	
+  totalUnitsHealed:	number,	
+  tripleKills:	number,	
+  trueDamageDealt:	number,	
+  trueDamageDealtToChampions:	number,	
+  trueDamageTaken:	number,	
+  turretKills:	number,	
+  turretTakedowns:	number,	
+  turretsLost:	number,	
+  unrealKills:	number,	
+  visionScore:	number,	
+  visionWardsBoughtInGame:	number,	
+  wardsKilled:	number,	
+  wardsPlaced:	number,	
+  win:	boolean,	
 }
+type Perks= {
+  statPerks:	PerkStats,
+  styles:	PerkStyle[]
+}
+type PerkStats = {
+  defense: number,	
+  flex: number,
+  offense: number
+}
+type PerkStyle={
+  description:	string,
+  selections: PerkStyleSelection,
+  style: number
+}
+
+type PerkStyleSelection = {
+  perk: number,
+  var1: number,	
+  var2: number,	
+  var3: number
+}
+
+type Team = {
+  bans:	Ban[],
+  objectives:	Objectives,
+  teamId:	number,
+  win:	boolean,
+}
+
+type Objectives = {
+  baron:	Objective,
+  champion:	Objective,	
+  dragon:	Objective,	
+  inhibitor:	Objective,	
+  riftHerald:	Objective,	
+  tower:	Objective,
+}
+
+type Objective = {
+  first: boolean,
+  kills: number
+}
+
+type Ban = {
+  championId: number,	
+  pickTurn: number
+}
+
 export type SummonerResponse = {
-  accountId : string;
-  profileIconId :	number	
-  revisionDate	: number;
+  accountId: string;
+  profileIconId: number;
+  revisionDate: number;
   name: string;
-  id	: string;
-  puuid	: string;
-  summonerLevel	: number;  
-}
+  id: string;
+  puuid: string;
+  summonerLevel: number;
+};
 
 export namespace SummonerMethods {
-  let base: string  = "lol/summoner/v4/summoners/";
-  export async function getSummoner (queryParams: SummonerQuery): Promise<SummonerResponse>{
-    base += queryParams.endpoint + `${queryParams.identifier}?api_key=${queryParams.apiKey}`;
-    return Methods.getSummoner(base, queryParams.route)
-  }
-}
-
-export namespace MatchesMethods {
-  let base: string  = "lol/match/v5/matches/";
-  export async function getMatchesFromPUUID (puuid: string, region: Route, apiKey: string): Promise<MatchList>{
-    base += `by-puuid/${puuid}/ids?api_key=${apiKey}`
-    return Methods.Request(base,region)
-  }
-}
-
-
-namespace Methods {
-  
-  export async function getSummoner(base: string, route: Route): Promise<SummonerResponse>{
-    return Request<SummonerResponse>(base,route);
-  }
-  export async function getMatches(base: string, route: Route): Promise<MatchList>{
-    return Request<MatchList>(base,route);
-  }
-  
-  export async function Request<T>(url: string, route:Route) {
-    return fetch(`https://${route}.api.riotgames.com/${url}`).then(response => {
-    if (!response.ok) {
-      throw new Error(response.statusText);
+  export async function getSummoner(
+    queryParams: ApiQueryParams
+    ): Promise<SummonerResponse> {
+      const base = `lol/summoner/v4/summoners/${queryParams.endpoint}${queryParams.identifier}?api_key=${queryParams.apiKey}`;
+      return Methods.Request<SummonerResponse>(base, queryParams.route);
     }
-    return response.json() as Promise<T>
-  })
-}
-}
+  }
+  
+  export namespace MatchesMethods {
+    export async function getMatchesList(
+      queryParams: MatchListQuery
+      ): Promise<MatchList> {
+        let query: string = "&";
+        if (queryParams.count !== undefined){
+          query += `&count=${queryParams.count}`
+        }
+        if (queryParams.start !== undefined){
+          query += `&start=${queryParams.start}`
+        }
+        const base = `lol/match/v5/matches/${queryParams.endpoint}${queryParams.identifier}/ids?api_key=${queryParams.apiKey}${query}`;
+        return Methods.Request<MatchList>(base, queryParams.route);
+      }
+      export async function getMatchFromMatchId(
+        queryParams: ApiQueryParams
+        ): Promise<Match> {
+          const base = `lol/match/v5/matches/${queryParams.identifier}?api_key=${queryParams.apiKey}`;
+          return Methods.Request<Match>(base, queryParams.route);
+        }
+      }
+      
+      namespace Methods {
+        export async function Request<T>(url: string, route: Route) {
+          return fetch(`https://${route}.api.riotgames.com/${url}`).then(
+          (response) => {
+            if (!response.ok) {
+              throw new Error(response.statusText);
+            }
+            return response.json() as Promise<T>;
+          }
+          );
+        }
+      }
+      
--- sources/index.ts
+++ sources/index.ts
@@ -1,33 +1,91 @@
 "use strict";
 
-import {SummonerMethods, Route, SummonerResponse, MatchesMethods, MatchList} from "./@types/index.js"
+import {
+    SummonerMethods,
+    Route,
+    SummonerResponse,
+    MatchesMethods,
+    MatchList,
+    Match,
+    MatchListQuery
+} from "./@types/index.js";
 
 export default class LolApi {
-  apiKey: string;
-  constructor(_apiKey: string) {
-    this.apiKey = _apiKey;
-  }
-
-  get summoner() {
-    return {
-      getSummonerBySummonerName: (summonerName:string , server: Route): Promise<SummonerResponse> =>
-        SummonerMethods.getSummoner({endpoint: 'by-name/',identifier: summonerName, apiKey: this.apiKey, route: server}),
-      getSummonerBySummonerId: (summonerId:string , server: Route): Promise<SummonerResponse> =>
-        SummonerMethods.getSummoner({endpoint: '', identifier: summonerId, apiKey: this.apiKey, route: server}),
-      getSummonerByAccountID: (accountID:string , server: Route): Promise<SummonerResponse> =>
-        SummonerMethods.getSummoner({endpoint: 'by-account/', identifier: accountID, apiKey: this.apiKey, route: server}),
-      getSummonerByPuuid: (puuid:string , server: Route): Promise<SummonerResponse> =>
-        SummonerMethods.getSummoner({endpoint: 'by-puuid/', identifier: puuid, apiKey: this.apiKey, route: server}),
+    apiKey: string;
+    constructor(_apiKey: string) {
+        this.apiKey = _apiKey;
     }
-  }
-  get match() {
-    return {
-      getMatchesFromPUUID: (puuid:string , region: Route): Promise<MatchList> =>
-        MatchesMethods.getMatchesFromPUUID(puuid, region, this.apiKey),
-    }
-  }
 
+    get summoner() {
+        return {
+            getSummonerBySummonerName: (
+                summonerName: string,
+                server: Route
+            ): Promise<SummonerResponse> =>
+                SummonerMethods.getSummoner({
+                    endpoint: "by-name/",
+                    identifier: summonerName,
+                    apiKey: this.apiKey,
+                    route: server
+                }),
+            getSummonerBySummonerId: (
+                summonerId: string,
+                server: Route
+            ): Promise<SummonerResponse> =>
+                SummonerMethods.getSummoner({
+                    endpoint: "",
+                    identifier: summonerId,
+                    apiKey: this.apiKey,
+                    route: server
+                }),
+            getSummonerByAccountID: (
+                accountID: string,
+                server: Route
+            ): Promise<SummonerResponse> =>
+                SummonerMethods.getSummoner({
+                    endpoint: "by-account/",
+                    identifier: accountID,
+                    apiKey: this.apiKey,
+                    route: server
+                }),
+            getSummonerByPuuid: (
+                puuid: string,
+                server: Route
+            ): Promise<SummonerResponse> =>
+                SummonerMethods.getSummoner({
+                    endpoint: "by-puuid/",
+                    identifier: puuid,
+                    apiKey: this.apiKey,
+                    route: server
+                })
+        };
+    }
+    get match() {
+        return {
+            getMatchesFromPUUID: (
+                {
+                    identifier: puuid,
+                    route: region,
+                    start: start,
+                    count: count
+                }: MatchListQuery
+            ): Promise<MatchList> =>
+                MatchesMethods.getMatchesList({
+                    endpoint: "by-puuid/",
+                    identifier: puuid,
+                    apiKey: this.apiKey,
+                    route: region,
+                    start: start,
+                    count: count
+                }),
+            getMatchesFromMatchId: (matchId: string, region: Route): Promise<Match> =>
+                MatchesMethods.getMatchFromMatchId({
+                    route: region,
+                    apiKey: this.apiKey,
+                    identifier: matchId
+                })
+        };
+    }
 }
 
-export {Route, SummonerResponse}
-
+export { Route, SummonerResponse };
